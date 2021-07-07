8a29d02dc3419dea58305cca5e3759c9fb34d34a
sitaramgarlapati <51022447+sitaramgarlapati@users.noreply.github.com>
2021-07-08 00:08:22 +0530

    initial commit

---
+++ SpecFlowCalculator.Specs/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,63 @@
+﻿using TechTalk.SpecFlow;
+using FluentAssertions;
+
+namespace SpecFlowCalculator.Specs.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly Calculator _calculator = new Calculator();
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+        private int _result;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _calculator.FirstNumber = number;
+            // _scenarioContext.Pending();
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+
+            _calculator.SecondNumber = number;
+            // _scenarioContext.Pending();
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            //implement act (action) logic
+
+            //_scenarioContext.Pending();
+            _result = _calculator.Add();
+        }
+
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            _result = _calculator.Subtract();
+            //ScenarioContext.Current.Pending();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            //implement assert (verification) logic
+            //_scenarioContext.Pending();
+            _result.Should().Be(result);
+        }
+
+       
+
+    }
+}
---
+++ SpecFlowCalculator/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SpecFlowCalculator
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            //throw new NotImplementedException();
+            return FirstNumber + SecondNumber;
+        }
+
+        public int Subtract()
+        {
+            //throw new NotImplementedException();
+            //return 0;
+            return FirstNumber - SecondNumber;
+        }
+
+    }
+}
